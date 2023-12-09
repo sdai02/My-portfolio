98d7e9a36da74edc2116c05170bfa119b953cc94
Stéphane Dai <127096387+sdai02@users.noreply.github.com>
2023-12-09 12:49:52 +0100

    fix a css style

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-
+var $scroll = document.querySelector('nav');
+var $project_one = document.getElementById('li__project-one');
+var $linkedin = document.getElementById('link__linkedin'); 
 
 window.addEventListener('scroll', function(){
-    var $scroll = document.querySelector('nav');
+    
     if(document.documentElement.scrollTop > 0){
         $scroll.classList.add('body__nav--scroll');
     } else{
@@ -9,4 +11,13 @@ window.addEventListener('scroll', function(){
     }
 
 })
-    
+  
+$project_one.addEventListener('click', function(event){
+    event.preventDefault();
+    window.open('https://lucent-sundae-056ed6.netlify.app/','_blank');
+});
+
+$linkedin.addEventListener('click', function(event){
+    event.preventDefault();
+    window.open('https://www.linkedin.com/in/stéphane-dai-7083351b6/');
+})
