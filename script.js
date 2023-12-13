8d57c68e279a22ff63b9fd802685432a8ee6fb89
Stéphane Dai <127096387+sdai02@users.noreply.github.com>
2023-12-13 12:29:11 +0100

    clean js code

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var $scroll = document.querySelector('nav');
 var $project_one = document.getElementById('li__project-one');
 var $project_two = document.getElementById('li__project-two');
 var $project_three = document.getElementById('li__project-three');
-var $linkedin = document.getElementById('link__linkedin'); 
+var $linkedin = document.getElementById('link__linkedin');
 let $carousel = document.querySelectorAll('.portfolio__carousel li');
 let $button_left = document.querySelector('.carousel__button-left');
 let $button_right = document.querySelector('.carousel__button-right');
@@ -10,7 +10,7 @@ let $button__menu = document.getElementById('button__menu');
 let $menu = document.getElementById('menu');
 
 window.addEventListener('scroll', function(){
-    
+
     if(document.documentElement.scrollTop > 0){
         $scroll.classList.add('body__nav--scroll');
     } else{
@@ -18,72 +18,98 @@ window.addEventListener('scroll', function(){
     }
 
 })
-  
-$project_one.addEventListener('click', function(event){
-    event.preventDefault();
-    window.open('https://lucent-sundae-056ed6.netlify.app/','_blank');
-});
 
-$project_two.addEventListener('click', function(event){
-    event.preventDefault();
-    window.open('https://github.com/Aboubakar2004/Projet_Site_Location/tree/st%C3%A9phane','_blank');
-});
+let windowOpen= (element, url) =>{
+    element.onclick = (event)=>{
+        event.preventDefault();
+        window.open(url,'_blank');
+    }
+}
+windowOpen($project_one,'https://lucent-sundae-056ed6.netlify.app/')
+windowOpen($project_two,'https://github.com/Aboubakar2004/Projet_Site_Location/tree/st%C3%A9phane')
+windowOpen($project_three,'https://6575c824ce77752fcddb564c--majestic-zuccutto-8ada61.netlify.app/')
+windowOpen($linkedin,'https://www.linkedin.com/in/stéphane-dai-7083351b6/')
 
-$project_three.addEventListener('click', function(event){
-    event.preventDefault();
-    window.open('https://6575c824ce77752fcddb564c--majestic-zuccutto-8ada61.netlify.app/','_blank');
-});
+// $project_one.addEventListener('click', function(event){
+//     event.preventDefault();
+//     window.open('https://lucent-sundae-056ed6.netlify.app/','_blank');
+//  });
 
-$linkedin.addEventListener('click', function(event){
-    event.preventDefault();
-    window.open('https://www.linkedin.com/in/stéphane-dai-7083351b6/');
-});
+// $project_two.addEventListener('click', function(event){
+//     event.preventDefault();
+//     window.open('https://github.com/Aboubakar2004/Projet_Site_Location/tree/st%C3%A9phane','_blank');
+// });
+
+// $project_three.addEventListener('click', function(event){
+//     event.preventDefault();
+//     window.open('https://6575c824ce77752fcddb564c--majestic-zuccutto-8ada61.netlify.app/','_blank');
+// });
+// $linkedin.addEventListener('click', function(event){
+//     event.preventDefault();
+//     window.open('https://www.linkedin.com/in/stéphane-dai-7083351b6/');
+// });
 
 
+
+
+
+let carouselOff = (index)=>{
+    index.classList.remove("carousel__li");
+    index.classList.add("carousel__li--off");
+}
+
+let carouselOn = (index)=>{
+    index.classList.remove('carousel__li--off');
+    index.classList.add("carousel__li");
+
+}
 
 $index = 0;
 
-$button_right.addEventListener('click', function(){ 
-    $carousel[$index].classList.remove("carousel__li");
-    $carousel[$index].classList.add("carousel__li--off");
+ $button_right.addEventListener('click', function(){
+    carouselOff($carousel[$index])
+     $index += 1;
 
-    $index += 1; 
+     if($index == $carousel.length){
+         $index = 0
+         carouselOn($carousel[$index])
+     }else{
+         carouselOn($carousel[$index])
+     }
+ });
 
-    if($index == $carousel.length){
-        $index = 0
-        $carousel[$index].classList.remove('carousel__li--off');
-        $carousel[$index].classList.add("carousel__li");
-    }else{
-        $carousel[$index].classList.remove('carousel__li--off');
-        $carousel[$index].classList.add("carousel__li");
-    } 
-});
 
-$button_left.addEventListener('click', function(){ 
-    $carousel[$index].classList.remove("carousel__li");
-    $carousel[$index].classList.add("carousel__li--off");
-    
+
+$button_left.addEventListener('click', () => {
+    carouselOff($carousel[$index])
     if($index == 0){
         $index = $carousel.length-1;
-        $carousel[$index].classList.remove('carousel__li--off');
-        $carousel[$index].classList.add("carousel__li");
+        carouselOn($carousel[$index])
     }else{
         $index -= 1;
-        $carousel[$index].classList.remove('carousel__li--off');
-        $carousel[$index].classList.add("carousel__li");
+        carouselOn($carousel[$index])
     }
-    
 });
+
+
+let menuOn = (on) => {
+    $menu.classList.remove('menu--off');
+    $menu.classList.add('menu');
+}
+
+let menuOff = (off) => {
+    $menu.classList.remove('menu');
+    $menu.classList.add('menu--off');
+}
+
 
 $on = 0;
 $button__menu.addEventListener('click',function(){
     if($on == 0){
         $on += 1;
-        $menu.classList.remove('menu--off');
-        $menu.classList.add('menu');
+        menuOn()
     }else{
         $on -= 1;
-        $menu.classList.remove('menu');
-        $menu.classList.add('menu--off');
+        menuOff()
     }
 })
